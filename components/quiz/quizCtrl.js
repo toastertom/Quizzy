cfdbdb6f071970b2e0a11e9dd9e8817c5c3e8f03
ToasterTom <tate.thomas5595@gmail.com>
2016-10-08 12:11:22 -0600

    Setup Services

--- components/quiz/quizCtrl.js
+++ components/quiz/quizCtrl.js
@@ -1,4 +1,4 @@
 angular.module('quizApp').controller('quizCtrl', function
-() {
+($scope, questions, quizService, $stateParams) {
 
 })
---
+++ components/services/quizService.js
@@ -0,0 +1,63 @@
+angular.module('quizApp').service('quizService',function
+($q) {
+
+  var quizSampleObj = {
+      'html': {
+        id: 1,
+        name: 'HTML',
+        questions: [{
+          id: 1,
+          title: 'Box-model order from outside in is: Content, Border, Margin, Padding (T/F)',
+          qtype: 'multiple',
+          choices: ['T', 'F'],
+          correct: 1
+        },
+          {
+            id: 2,
+            title: 'Which is not a semantic html element?',
+            qtype: 'multiple',
+            choices: ['header', 'div', 'footer', 'article'],
+            correct: 1
+          }]
+      },
+      'angular': {
+        id: 2,
+        name: 'Angular',
+        questions: [{
+          id: 1,
+          title: 'DOM manipulation should be performed in an angular directive? (T/F)',
+          qtype: 'multiple',
+          choices: ['T', 'F'],
+          correct: 0
+        },
+              {
+                  id: 2,
+                  title: 'Which is not a valid option for a directive?',
+                  qtype: 'multiple',
+                  choices: ['transclude', 'link', 'scope', 'raccoon'],
+                  correct: 3
+              },
+              {
+                  id: 3,
+                  title: 'ng-click is a built-in angular _____.',
+                  qtype: 'blank',
+                  correct: 'directive'
+              },
+              {
+                  id: 4,
+                  title: 'DOM manipulation should be performed in an angular directive? (T/F)',
+                  qtype: 'multiple',
+                  choices: ['T', 'F'],
+                  correct: 0
+              },
+              {
+                  id: 5,
+                  title: 'Which is not a valid option for a directive?',
+                  qtype: 'multiple',
+                  choices: ['transclude', 'link', 'scope', 'The frenzied scratching of a rabid badger'],
+                  correct: 3
+              }]
+      }
+    };
+
+})
